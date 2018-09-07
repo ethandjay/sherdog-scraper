80eb16137bad5bf0d52c0fd01fae628069e21013
Ethan Jaynes <ethandjay@gmail.com>
2018-09-07 17:22:43 -0500

    event pagination, edge case fixes, changed how to get event title

--- sherdog.py
+++ sherdog.py
@@ -15,134 +15,161 @@ __version__ = "0.1.0"
 
 
 def strip_method(full_method):
-	match = re.search(r'\s*(.*)\s\((.*)\)', full_method)
-	method = match.group(1)
-	method_by = match.group(2)
 
-	return (method, method_by)
+    match = re.search(r'\s*(.*)\s\((.*)\)', full_method)
+    if match:
+        method = match.group(1)
+        method_by = match.group(2)
+    else:
+        method = full_method
+        method_by = "N/A"
+
+
+    return (method, method_by)
 
 def request_event(url):
 
-	base_url = "http://www.sherdog.com"
-	r = requests.get(base_url + url)
-	
-	event_soup = BeautifulSoup(r.text, "lxml")
+    base_url = "http://www.sherdog.com"
+    print(base_url + url)
+    r = requests.get(base_url + url)
+    
+    event_soup = BeautifulSoup(r.text, "lxml")
 
-	""" 
-	Match Return Schema:
+    """ 
+    Match Return Schema:
 
-	{
-		winner:
-		loser:
-		method:
-		method_by:
-		referee:
-		round_num:
-		time:
-		event:
-		date:
-		location:
-	}
-	"""
-	match_return = []
+    {
+        winner:
+        loser:
+        method:
+        method_by:
+        referee:
+        round_num:
+        time:
+        event:
+        date:
+        location:
+    }
+    """
+    match_return = []
 
-	# Get event info
-	event = event_soup.select('.section_title')[0].h1.span.contents[0]
-	date = event_soup.select('.authors_info')[0].select('.date')[0].contents[-1]
-	location = event_soup.find_all('span', {"itemprop" : "location"})[0].string
+    # Get event info
+    event = event_soup.find_all('meta', {"property" : "og:title"})[0]['content']
+    date = event_soup.select('.authors_info')[0].select('.date')[0].contents[-1]
+    location = event_soup.find_all('span', {"itemprop" : "location"})[0].string
 
-	# Get and parse main fight
-	winner = event_soup.select('.fight')[0].select('.left_side')[0].h3.span.string
-	loser = event_soup.select('.fight')[0].select('.right_side')[0].h3.span.string
+    if "CANCELED" in location.upper():
+        return None
 
-	details = event_soup.select('.resume')[0].find('tr').find_all('td')
-	full_method = details[1].contents[-1]
+    # Get and parse main fight
+    winner = event_soup.select('.fight')[0].select('.left_side')[0].h3.span.string
+    loser = event_soup.select('.fight')[0].select('.right_side')[0].h3.span.string
 
-	method, method_by = strip_method(full_method)
+    details = event_soup.select('.resume')[0].find('tr').find_all('td')
+    full_method = details[1].contents[-1]
 
-	referee = details[2].contents[-1].lstrip()
-	round_num = details[3].contents[-1].lstrip()
-	time = details[4].contents[-1].lstrip()
+    method, method_by = strip_method(full_method)
 
-	match_return.append({
-		'winner': winner,
-		'loser': loser,
-		'method': method,
-		'method_by': method_by,
-		'referee': referee,
-		'round_num': round_num,
-		'time': time,
-		'event': event,
-		'date': date,
-		'location': location
-	})
+    referee = details[2].contents[-1].lstrip()
+    round_num = details[3].contents[-1].lstrip()
+    time = details[4].contents[-1].lstrip()
 
-	match_list = event_soup.select('.event_match')[0].div.table.tbody.find_all('tr')
+    match_return.append({
+        'winner': winner,
+        'loser': loser,
+        'method': method,
+        'method_by': method_by,
+        'referee': referee,
+        'round_num': round_num,
+        'time': time,
+        'event': event,
+        'date': date,
+        'location': location
+    })
 
-	# Pop header tr
-	match_list.pop(0)
+    match_list = event_soup.select('.event_match')[0].div.table.tbody.find_all('tr')
 
-	for match in match_list:
-		contents = match.contents
-		fighters = (contents[3], contents[7])
+    # Pop header tr
+    match_list.pop(0)
 
-		winner = fighters[0].div.a.span.string
-		loser = fighters[1].div.a.span.string
+    for match in match_list:
+        contents = match.contents
+        fighters = (contents[3], contents[7])
 
-		details = contents[9].contents
-		full_method = details[0]
+        winner = fighters[0].div.a.span.string
+        loser = fighters[1].div.a.span.string
 
-		method, method_by = strip_method(full_method)
+        details = contents[9].contents
+        full_method = details[0]
 
-		referee = details[-1].string
-		round_num = contents[11].string
-		time = contents[13].string
+        method, method_by = strip_method(full_method)
 
-		match_return.append({
-			'winner': winner,
-			'loser': loser,
-			'method': method,
-			'method_by': method_by,
-			'referee': referee,
-			'round_num': round_num,
-			'time': time,
-			'event': event,
-			'date': date,
-			'location': location
-		})
+        referee = details[-1].string
+        round_num = contents[11].string
+        time = contents[13].string
 
-	print("Done processing event")
-	return match_return
-	
+        match_return.append({
+            'winner': winner,
+            'loser': loser,
+            'method': method,
+            'method_by': method_by,
+            'referee': referee,
+            'round_num': round_num,
+            'time': time,
+            'event': event,
+            'date': date,
+            'location': location
+        })
+
+    print("Done processing event")
+    return match_return
+    
 
 
 
 def main():
 
-    ufc_page = requests.get("http://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2")
-    ufc_page_soup = BeautifulSoup(ufc_page.text, "lxml")
+    
+    
+    event_links = []
+    k = 1
+    while True:
 
-    recent_list = ufc_page_soup.select('div#recent_tab')[0].table
+        ufc_page = requests.get(f"http://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2/recent-events/{k}")
 
-    recent_list = recent_list.find_all('tr')
+        ufc_page_soup = BeautifulSoup(ufc_page.text, "lxml")
 
-    # Remove header tr
-    recent_list.pop(0)
+        recent_list = ufc_page_soup.select('div#recent_tab')[0].table
 
-    # String event links from onlick attr
-    event_links = [row.get('onclick')[19:][:-2] for row in recent_list]
+        recent_list = recent_list.find_all('tr')
+
+        # If only header, no content
+        if len(recent_list) == 1:
+            break;
+
+        # Remove header tr
+        recent_list.pop(0)
+
+        # String event links from onlick attr
+        event_links.extend([row.get('onclick')[19:][:-2] for row in recent_list])
+
+        k = k + 1;
+
+
 
     num_cores = multiprocessing.cpu_count()
-    event_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(request_event)(link) for link in event_links)
+    event_list = Parallel(n_jobs=num_cores, backend='threading')(delayed(request_event)(link) for link in event_links[:200])
+    event_list = filter(None, event_list)
 
     with open('matches.csv', 'w', newline='') as f:
-	    fnames = ['winner','loser','method','method_by','referee','round_num','time','event','date','location']
-	    writer = csv.DictWriter(f, fieldnames=fnames)    
+        fnames = ['winner','loser','method','method_by','referee','round_num','time','event','date','location']
+        writer = csv.DictWriter(f, fieldnames=fnames)    
 
-	    writer.writeheader()
-	    for event in event_list:
-	    	for match in event:
-	    		writer.writerow(match)
+
+        writer.writeheader()
+        for event in event_list:
+            for match in event:
+                writer.writerow(match)
 
 
 
